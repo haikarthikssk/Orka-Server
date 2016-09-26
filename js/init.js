bf7092f991c95b7e3d7e8348a663618f7da3046d
karthik <haikarthikssk@gmail.com>
2016-09-26 22:59:37 +0530

    Orka Webpage

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+	$(document).ready(function(){
+	$('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	})
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
